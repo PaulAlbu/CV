33772f390f3af13ddd8d2258811053a88ae416a6
PaulAlbu <39088434+PaulAlbu@users.noreply.github.com>
2020-07-16 01:21:54 +0300

    Added JS for Home page header

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-$(window).on("scroll", function() {
-    if($(window).scrollTop() > 50) {
-        $(".header").addClass("active");
-    } else {
-        //remove the background property so it comes transparent again (defined in your css)
-       $(".header").removeClass("active");
-    }
+$(function() {
+    $(window).on("scroll", function() {
+        if($(window).scrollTop() > 50) {
+            $(".header").addClass("active");
+        } else {
+            //remove the background property so it comes transparent again (defined in your css)
+           $(".header").removeClass("active");
+        }
+    });
 });
